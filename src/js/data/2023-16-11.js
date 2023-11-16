695a22cfb0a1eb8ec3b119b84d4a8d371699337a
BellDHamster <150895665+BellDHamster@users.noreply.github.com>
2023-11-17 01:13:46 +0700

    Update 2023-16-11.js

--- src/js/data/2023-16-11.js
+++ src/js/data/2023-16-11.js
@@ -47,7 +47,7 @@ dataSet[dataSetVersion].options = [
 dataSet[dataSetVersion].characterData = [
   {
     name: "Soyotaki",
-    img: "Soyotaki.jpg",
+    img: "Soyotaki.png",
     opts: {
       bands: ["mygo","cryc"],
       positions: ["ab"],
